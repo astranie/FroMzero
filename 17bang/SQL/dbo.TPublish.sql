ca7f9d379177183ca9784f17f8eff0e284ef9830
nie ze <nnnzxnnn@outlook.com>
2019-07-17 22:38:45 +0800

    SQLServer学习

---
+++ 17bang/SQL/dbo.TPublish.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TPublish]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NTEXT NOT NULL, 
+    [Description] NTEXT NULL, 
+    [Keyword] NCHAR(30) NULL, 
+    [HelperName] NCHAR(10) NULL, 
+    [Money] INT NULL
+)
---
+++ 17bang/SQL/dbo.TUer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TUer] (
+    [Id]              INT        NOT NULL,
+    [UserName]        NCHAR (20) NOT NULL,
+    [Password]        NCHAR (20) NOT NULL,
+    [CorrectPassword] NCHAR (20) NOT NULL,
+    [CreatTime]       DATETIME   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
